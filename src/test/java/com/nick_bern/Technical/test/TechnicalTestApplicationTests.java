56c65e0cd5f4a395b44838daf5ae8117c0c3a2d8
DevNico2905 <nicobernal2905@gmail.com>
2026-03-18 10:02:03 -0500

    Initial commit

---
+++ src/test/java/com/nick_bern/Technical/test/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick_bern.Technical.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
