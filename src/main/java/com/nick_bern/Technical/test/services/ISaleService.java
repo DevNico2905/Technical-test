1e65806e27496e44cf407d9c468e53e65fdb3bc2
DevNico2905 <nicobernal2905@gmail.com>
2026-03-19 08:35:26 -0500

    Sale services Interface

--- src/main/java/com/nick_bern/Technical/test/services/ISaleService.java
+++ src/main/java/com/nick_bern/Technical/test/services/ISaleService.java
@@ -1,4 +1,16 @@
 package com.nick_bern.Technical.test.services;
 
+import com.nick_bern.Technical.test.dtos.SaleDTO;
+import java.util.List;
+
 public interface ISaleService {
+
+    List<SaleDTO> getAllSales();
+
+    SaleDTO createSale(SaleDTO newSale);
+
+    SaleDTO updateSale(Long idSale, SaleDTO sale);
+
+    void deleteSale(Long idSale);
+
 }
