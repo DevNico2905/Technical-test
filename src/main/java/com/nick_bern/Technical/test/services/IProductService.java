dd5ab4aa9730b7a979fe25068a0d22f1fd413253
DevNico2905 <nicobernal2905@gmail.com>
2026-03-19 08:32:51 -0500

    Product services Interface

--- src/main/java/com/nick_bern/Technical/test/services/IProductService.java
+++ src/main/java/com/nick_bern/Technical/test/services/IProductService.java
@@ -1,4 +1,16 @@
 package com.nick_bern.Technical.test.services;
 
+import com.nick_bern.Technical.test.dtos.ProductDTO;
+
+import java.util.List;
+
 public interface IProductService {
+
+    List<ProductDTO> getAllProducts();
+
+    ProductDTO createProduct(ProductDTO newProduct);
+
+    ProductDTO updateProduct(Long idProduct, ProductDTO product);
+
+    void deleteProduct(Long idProduct);
 }
