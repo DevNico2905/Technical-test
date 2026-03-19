503b87c51ca7db5728f7dd9279ef02cf135f5942
DevNico2905 <nicobernal2905@gmail.com>
2026-03-19 08:29:14 -0500

    Store services Interface

--- src/main/java/com/nick_bern/Technical/test/services/IStoreService.java
+++ src/main/java/com/nick_bern/Technical/test/services/IStoreService.java
@@ -1,4 +1,17 @@
 package com.nick_bern.Technical.test.services;
 
+import com.nick_bern.Technical.test.dtos.StoreDTO;
+
+import java.util.List;
+
 public interface IStoreService {
+
+    List<StoreDTO> findAllStores();
+
+    StoreDTO createStore(StoreDTO newStore);
+
+    StoreDTO updateStore(Long idStore, StoreDTO store);
+
+    void deleteStore(Long idStore);
+
 }
