050cc3d9931cc05351fba878fd9ce4361b9159a3
DevNico2905 <nicobernal2905@gmail.com>
2026-03-20 11:20:29 -0500

    Using personals exceptions

---
+++ src/main/java/com/nick_bern/Technical/test/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.nick_bern.Technical.test.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message){
+        super(message);
+    }
+}
