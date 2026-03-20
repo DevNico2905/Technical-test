bbc24615a1b8df87fffd89adbf9d0743eefa73cd
DevNico2905 <nicobernal2905@gmail.com>
2026-03-20 11:41:18 -0500

    Sale Service on process...

--- src/main/java/com/nick_bern/Technical/test/services/Impl/SaleService.java
+++ src/main/java/com/nick_bern/Technical/test/services/Impl/SaleService.java
@@ -1,16 +1,25 @@
 package com.nick_bern.Technical.test.services.Impl;
 
 import com.nick_bern.Technical.test.dtos.SaleDTO;
+import com.nick_bern.Technical.test.exceptions.NotFoundException;
+import com.nick_bern.Technical.test.mappers.Mappers;
+import com.nick_bern.Technical.test.repositories.SaleRepository;
 import com.nick_bern.Technical.test.services.ISaleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class SaleService implements ISaleService {
+
+    private final SaleRepository saleRepo;
+
     @Override
     public List<SaleDTO> getAllSales() {
-        return List.of();
+        return saleRepo.findAll()
+                .stream().map(Mappers::toDTO).toList();
     }
 
     @Override
@@ -25,6 +34,10 @@ public class SaleService implements ISaleService {
 
     @Override
     public void deleteSale(Long idSale) {
-
+        if (!saleRepo.existsById(idSale)){
+            throw new NotFoundException("Sale with id: " + idSale + " not found!");
+        } else {
+            saleRepo.deleteById(idSale);
+        }
     }
 }
